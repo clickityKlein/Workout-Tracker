dc4622d122c03dbabacb22069974cdfb92959fd6
Carl Klein <carlj.klein@gmail.com>
2024-06-11 20:52:58 -0600

    config changes 2

--- workouts/__init__.py
+++ workouts/__init__.py
@@ -4,17 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from flask_login import LoginManager
-# import config
+# import config # removed for deployment
 
 app = Flask(__name__)
 
 def configure():
     load_dotenv()
 
-# app.config['SECRET_KEY'] = config.get_secret_key()
+# app.config['SECRET_KEY'] = config.get_secret_key() # changed for deployment
+app.config['SECRET_KEY'] = SECRET_KEY
 
 ### database setup ###
-# app.config['SQLALCHEMY_DATABASE_URI'] = config.get_db_url()
+# app.config['SQLALCHEMY_DATABASE_URI'] = config.get_db_url() # changed for deployment
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
