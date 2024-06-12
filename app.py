9b906301c14aa91d83389a88b21ffd59e77d3734
Carl Klein <carlj.klein@gmail.com>
2024-06-11 21:38:52 -0600

    change to deployment app.py settings

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from workouts import app
 
 if __name__ == '__main__':
     # deploy settings
-    # app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000)
     
     # test settings
-    app.run()
+    # app.run()
